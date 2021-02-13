cdf8bfe2c0bdcb7b3f3c8ea32f0cb7205ebb038a
rohangrge <rohangrge@gmail.com>
2021-02-14 01:42:54 +0530

    add cpu temp

--- mail_bot.py
+++ mail_bot.py
@@ -1,5 +1,6 @@
 import flask
 from flask import jsonify
+import os
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,6 +9,13 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"status": "ok"})
+
+
+@app.route('/cputemp', methods=['GET'])
+def home():
+    temp = os.popen("vcgencmd measure_temp").readline()
+    return jsonify({"cpu": (temp.replace("temp=", "")})
+
 
 
 app.run()
